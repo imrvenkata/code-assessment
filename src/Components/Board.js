24d4ee1dc1e9d3b363a4b1fd5bf5581df8028f34
imrvenkata <venkatap0725@gmail.com>
2021-04-07 05:47:40 +0530

    possbile winning lines are

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -63,6 +63,21 @@ class Board extends React.Component {
    */
 
   /**
+   * 
+   * 
+   *  Possible winning lines are
+   *  [0, 1, 2]
+   *  [3, 4, 5]
+   *  [6, 7, 8]
+   *  [0, 3, 6]
+   *  [1, 4, 7]
+   *  [2, 5, 8]
+   *  [0, 4, 8]
+   *  [2, 4, 6]
+   *
+   */
+
+  /**
    *
    * @param {*} play
    * @returns true or false [win or not]
