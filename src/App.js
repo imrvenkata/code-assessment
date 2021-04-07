f71f8737d8887b8352095f27ad563718d1efa9c2
imrvenkata <venkatap0725@gmail.com>
2021-04-07 05:42:53 +0530

    created the navbar component to show the stats

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-import React from 'react'
+import React from "react";
 import { render } from "@testing-library/react";
 import Createplayers from "./Components/Createplayers";
+import Navbar from "./Components/Navbar";
 class App extends React.Component {
-
+  state = {
+    playerName1: "player1",
+    playerName2: "player2",
+    player1: 0,
+    player2: 0,
+    ties: 0,
+  };
   render() {
-    return(
+    const { playerName1, playerName2, player1, player2, ties } = this.state;
+    return (
       <div className="App">
+        <Navbar
+          playerName1={playerName1}
+          playerName2={playerName2}
+          player1={player1}
+          player2={player2}
+          ties={ties}
+        />
         <Createplayers></Createplayers>
       </div>
-      
-    )
+    );
   }
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./../Styles/style.scss";
+class Navbar extends React.Component {
+  render() {
+    const { player1, player2, ties, playerName1, playerName2 } = this.props;
+    return (
+      <div className="navbar">
+        <div className="title">
+          <h1> GAP - TicTacToe </h1>
+        </div>
+        <div className="statistics">
+          <h2> {playerName1} : {player1} wins </h2>
+          <h2> {playerName2} : {player2} wins </h2>
+          <h2> ties : {ties} </h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
