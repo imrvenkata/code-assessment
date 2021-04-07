72b5f211f118d2d62a0b8dd8bcc43430e9e54ed7
imrvenkata <venkatap0725@gmail.com>
2021-04-07 05:40:30 +0530

    create players component to get the usernames

---
+++ src/Components/Createplayers.js
@@ -0,0 +1,57 @@
+import React from "react";
+import "./../Styles/style.scss";
+class Createplayers extends React.Component {
+  state = {
+    player1: "",
+    player2: "",
+  };
+
+  handleChange = (event) => {
+    console.log("Create player handleChange");
+    console.log(event.target.value);
+    let player = event.target.id;
+    this.setState({ [player]: event.target.value });
+  };
+
+  handleClick = (event) => {
+    event.preventDefault();
+    const { player1, player2 } = this.state;
+    // setting the player_names recieved from the form
+    this.props.names(player1, player2);
+  };
+
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="screen">
+          <form>
+            <div className="form-group">
+              <label> Create player1</label>
+              <input
+                type="name"
+                id="player1"
+                className="form-control"
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="form-group">
+              <label> Create player2</label>
+              <input
+                type="name"
+                id="player2"
+                className="form-control"
+                onChange={this.handleChange}
+              />
+            </div>
+
+            <button onClick={this.handleClick} className="btn btn-primary">
+              Let's play
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Createplayers;
